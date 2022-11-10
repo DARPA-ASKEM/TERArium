dae5d90010a3f43bb7cf277fb6c5b2f59df4dbf3
mwdchang <mwdchang@gmail.com>
2022-11-10 17:34:59 -0500

    feat: stub project/model/simulation views

--- packages/client/hmi-client/src/router/index.ts
+++ packages/client/hmi-client/src/router/index.ts
@@ -3,20 +3,30 @@ import HomeView from '@/views/Home.vue';
 import ResponsiveMatrixCells from '@/components/ResponsiveMatrixCells.vue';
 import TA2Playground from '@/views/TA2Playground.vue';
 import TheiaView from '@/views/theia.vue';
+import ModelView from '@/views/Model.vue';
+import ProjectViw from '@/views/Project.vue';
+import SimulationView from '@/views/Simulation.vue';
+
 import { computed } from 'vue';
 
 export enum RoutePath {
 	Home = '/',
 	Results = '/results',
 	Ta2Playground = '/ta2-playground',
-	Theia = '/theia'
+	Theia = '/theia',
+	Project = '/project',
+	Model = '/model',
+	Simulation = '/simulation'
 }
 
 const routes = [
 	{ path: RoutePath.Home, component: HomeView },
 	{ path: RoutePath.Results, component: ResponsiveMatrixCells },
 	{ path: RoutePath.Ta2Playground, component: TA2Playground },
-	{ path: RoutePath.Theia, component: TheiaView }
+	{ path: RoutePath.Theia, component: TheiaView },
+	{ path: RoutePath.Project, component: ProjectViw },
+	{ path: RoutePath.Model, component: ModelView },
+	{ path: RoutePath.Simulation, component: SimulationView }
 ];
 
 const router = createRouter({
---
+++ packages/client/hmi-client/src/views/Model.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div>Model view</div>
+</template>
